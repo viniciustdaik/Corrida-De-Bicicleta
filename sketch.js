9809d098dea2cad52c03847d3153a254bd874c8e
viniciustdaik <96190816+viniciustdaik@users.noreply.github.com>
2022-03-13 11:39:37 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,17 +1,17 @@
-var path,mainCyclist;
-var player1,player2,player3, player4, obstacle1, obstacle2, obstacle3;
-var pathImg,mainRacerImg1,mainRacerImg2;
+var path, mainCyclist;
+var player1, player2,player3, player4, obstacle1, obstacle2, obstacle3;
+var pathImg, mainRacerImg1, mainRacerImg2;
 
-var oppPink1Img,oppPink2Img;
-var oppYellow1Img,oppYellow2Img;
-var oppRed1Img,oppRed2Img;
-var gameOverImg,cycleBell;
+var oppPink1Img, oppPink2Img;
+var oppYellow1Img, oppYellow2Img;
+var oppRed1Img, oppRed2Img;
+var gameOverImg, cycleBell;
 var obstacle1img, obstacle2img, obstacle3img;
 
 var pinkCG, yellowCG,redCG, cyanCG, obstacles1G, obstacles2G, obstacles3G;
 
-var END =0;
-var PLAY =1;
+var END = 0;
+var PLAY = 1;
 var gameState = PLAY;
 
 var distance=0;
@@ -23,43 +23,47 @@ var hitbox;
 
 function preload(){
   pathImg = loadImage("Road.png");
-  mainRacerImg1 = loadAnimation("mainPlayer1.png","mainPlayer2.png");
+  mainRacerImg1 = loadAnimation("mainPlayer1.png", "mainPlayer2.png");
   mainRacerImg2= loadAnimation("mainPlayer3.png");
   
-  oppPink1Img = loadAnimation("opponent1.png","opponent2.png");
-  oppPink2Img = loadAnimation("opponent3.png");
+  oppPink1Img = loadAnimation("./obstacles-opponents/pink/opponent1.png", 
+  "./obstacles-opponents/pink/opponent2.png");
+  oppPink2Img = loadAnimation("./obstacles-opponents/pink/opponent3.png");
   
-  oppYellow1Img = loadAnimation("opponent4.png","opponent5.png");
-  oppYellow2Img = loadAnimation("opponent6.png");
+  oppYellow1Img = loadAnimation("./obstacles-opponents/yellow/opponent4.png", 
+  "./obstacles-opponents/yellow/opponent5.png");
+  oppYellow2Img = loadAnimation("./obstacles-opponents/yellow/opponent6.png");
   
-  oppRed1Img = loadAnimation("opponent7.png","opponent8.png");
-  oppRed2Img = loadAnimation("opponent9.png");
+  oppRed1Img = loadAnimation("./obstacles-opponents/red/opponent7.png", 
+  "./obstacles-opponents/red/opponent8.png");
+  oppRed2Img = loadAnimation("./obstacles-opponents/red/opponent9.png");
   
   cycleBell = loadSound("bell.mp3");
   gameOverImg = loadImage("gameOver.png");
 
-  oppCyan1Img = loadAnimation("opponent10.png", "opponent11.png");
-  oppCyan2Img = loadAnimation("opponent12.png");
+  oppCyan1Img = loadAnimation("./obstacles-opponents/cyan/opponent10.png", 
+  "./obstacles-opponents/cyan/opponent11.png");
+  oppCyan2Img = loadAnimation("./obstacles-opponents/cyan/opponent12.png");
 
-  obstacle1img = loadImage("obstacle1.png");
-  obstacle2img = loadImage("obstacle2.png");
-  obstacle3img = loadImage("obstacle3.png");
+  obstacle1img = loadImage("./obstacles-opponents/obstacle1.png");
+  obstacle2img = loadImage("./obstacles-opponents/obstacle2.png");
+  obstacle3img = loadImage("./obstacles-opponents/obstacle3.png");
 }
 
 function setup(){
   
 createCanvas(windowWidth, windowHeight);//1200, 300
-// Moving background
+//Moving background.
 path=createSprite(width/2,height/2);
 path.addImage("pathimg", pathImg);
 path.velocityX = -5;
 
-//creating boy running
+//Creating boy running.
 mainCyclist  = createSprite(70,150);
 mainCyclist.addAnimation("SahilRunning",mainRacerImg1);
 mainCyclist.scale=0.07;
 
-//set collider for mainCyclist
+//Set Collider for mainCyclist.
 mainCyclist.setCollider("rectangle", 0, 0, 40, 1405); //main.
 //mainCyclist.setCollider("rectangle", 0, 390, 40, 605); //can go up can't down.
 //mainCyclist.setCollider("rectangle",0,0,40,40); 
@@ -279,30 +283,30 @@ function draw() {
 }
 
 function pinkCyclists(){
-        player1 =createSprite(windowWidth+1100,Math.round(random(50, windowHeight-50)));//1100, (random)50, 250
-        player1.scale =0.06;
-        player1.velocityX = -(6 + 2*distance/150);
-        player1.addAnimation("opponentPlayer1",oppPink1Img);
-        player1.setLifetime=170;
-        pinkCG.add(player1);
+  player1 =createSprite(windowWidth+1100,Math.round(random(50, windowHeight-50)));//1100, (random)50, 250
+  player1.scale =0.06;
+  player1.velocityX = -(6 + 2*distance/150);
+  player1.addAnimation("opponentPlayer1",oppPink1Img);
+  player1.setLifetime=170;
+  pinkCG.add(player1);
 }
 
 function yellowCyclists(){
-        player2 =createSprite(windowWidth+1100,Math.round(random(50, windowHeight-50)));//1100, (random)50, 250
-        player2.scale =0.06;
-        player2.velocityX = -(6 + 2*distance/150);
-        player2.addAnimation("opponentPlayer2",oppYellow1Img);
-        player2.setLifetime=170;
-        yellowCG.add(player2);
+  player2 =createSprite(windowWidth+1100,Math.round(random(50, windowHeight-50)));//1100, (random)50, 250
+  player2.scale =0.06;
+  player2.velocityX = -(6 + 2*distance/150);
+  player2.addAnimation("opponentPlayer2",oppYellow1Img);
+  player2.setLifetime=170;
+  yellowCG.add(player2);
 }
 
 function redCyclists(){
-        player3 =createSprite(windowWidth+1100,Math.round(random(50, windowHeight-50)));//1100, (random)50, 250
-        player3.scale =0.06;
-        player3.velocityX = -(6 + 2*distance/150);
-        player3.addAnimation("opponentPlayer3",oppRed1Img);
-        player3.setLifetime=170;
-        redCG.add(player3);
+  player3 =createSprite(windowWidth+1100,Math.round(random(50, windowHeight-50)));//1100, (random)50, 250
+  player3.scale =0.06;
+  player3.velocityX = -(6 + 2*distance/150);
+  player3.addAnimation("opponentPlayer3",oppRed1Img);
+  player3.setLifetime=170;
+  redCG.add(player3);
 }
 
 function cyanCyclists(){
